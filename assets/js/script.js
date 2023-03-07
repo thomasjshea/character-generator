38203a1508e1ea53d1716fb7b31a63b11c14f6fa
ian heap <ianheap77@gmail.com>
2023-03-06 21:40:18 -0500

    used Js to create array for races

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,25 @@
 let classes = 'https://www.dnd5eapi.co/api/classes'
 let races = 'https://www.dnd5eapi.co/api/races'
 let abilityScores = 'https://www.dnd5eapi.co/api/ability-scores'
+let raceE1 = document.getElementById('race-select')
 
+function populateRaces() {
+    
+    fetch(races)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function(data) {
+        console.log(data)
+    for(let i=0; i< data.results.length; i++){
+        let raceOption =document.createElement('option')
+        raceOption.classList = 'dropdown-option'
+        raceOption.value = data.results[i].index
+        console.log(raceOption.value)
+        raceOption.innerHTML= data.results[i].name
+        raceE1.appendChild(raceOption)
+    }
+    })
+}
 
+populateRaces()
