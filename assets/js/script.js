b9b7f4aeb261ace9a65b5f8087819e584ad69459
ian heap <ianheap77@gmail.com>
2023-03-06 21:55:29 -0500

    retry merge with thomas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 let classes = 'https://www.dnd5eapi.co/api/classes'
 let races = 'https://www.dnd5eapi.co/api/races'
 let abilityScores = 'https://www.dnd5eapi.co/api/ability-scores'
-let classEl = document.getElementById('class-select')
+let raceEl = document.getElementById('race-select')
 
 // Populate Classes to Dropdown Menu
-function populateClasses() {
+function populateRaces() {
 
     fetch(classes)
         .then(function (response) {
@@ -13,14 +13,15 @@ function populateClasses() {
         .then(function(data) {
             console.log(data)
             for (let i = 0; i < data.results.length; i++) {
-                let classOption = document.createElement('option')
-                classOption.classList = 'dropdown-option'
-                classOption.value = data.results[i].index
-                console.log(classOption.value)
-                classOption.innerHTML = data.results[i].name
-                classEl.appendChild(classOption)
+                let raceOption = document.createElement('option')
+                raceOption.classList = 'dropdown-option'
+                raceOption.value = data.results[i].index
+                console.log(raceOption.value)
+                raceOption.innerHTML = data.results[i].name
+                raceEl.appendChild(raceOption)
             }
         })
 let raceE1 = document.getElementById('race-select')
 
 }
+populateRaces()
