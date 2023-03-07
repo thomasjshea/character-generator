cf6e3feb71c0b9fa951a530c18dc1ff0d84582a2
thomasjshea <thomasjshea89@gmail.com>
2023-03-06 18:29:31 -0600

    created access to api

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+let classes = 'https://www.dnd5eapi.co/api/classes'
+let races = 'https://www.dnd5eapi.co/api/races'
+let abilityScores = 'https://www.dnd5eapi.co/api/ability-scores'
+
+// create dropdown menu for races and classes
+let classMenu = document.body.createElement(form)
