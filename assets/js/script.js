93da6168e2e1258e6fd58dea7133b57d4b746832
Brain Atoms <badams6453@gmail.com>
2023-03-07 20:43:14 -0500

    added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,26 @@
-const saveBtn = document.getElementsByClassName("button");
+// array including object of character features for saved characters 
+characterFeatures = [{
+    name:'',
+    class:'',
+    race:'',
+    stats:''
+}];
 
-const character = document.getElementById("character");
-
-characterArr = ["Arthur"];
-
+// saves users generated character and adds to local storage
 function saveCharacter() {
   console.log("saveCharacter");
-  localStorage.setItem("character", characterArr);
+  localStorage.setItem("character", JSON.stringify(characterArr));
 }
 saveCharacter();
 
+// loads saved character and their features for user
 function retrieveSavedCharacter() {
-  // localStorage.getItem('character')
   console.log("retrieveSavedCharacter");
 
   for (i = 0; i < characterArr.length; i++) {
     let character = document.getElementById("character-list");
     let savedCharacter = document.createElement("li");
-    savedCharacter.innerText = characterArr[i];
+    savedCharacter.innerText = JSON.stringify(characterArr[i]);
     character.appendChild(savedCharacter);
   }
 }
