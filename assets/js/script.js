c86553f5037f7e8a5f66e87ee781417817056226
Brain Atoms <badams6453@gmail.com>
2023-03-08 12:18:49 -0500

    made possible to save class and race to local storage, load character features from load button and added clear button to clear saved characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,132 @@
-const character = document.getElementById("character");
-// array of objects containing features of saved characters
-characterArr = [{
-    name:'',
-    class:'',
-    race:'',
-    stats:''
-}];
+// Declare variables
+let classes = 'https://www.dnd5eapi.co/api/classes'
+let races = 'https://www.dnd5eapi.co/api/races'
+let abilityScores = 'https://www.dnd5eapi.co/api/ability-scores'
+let raceEl = document.getElementById('race-select')
+let classEl = document.getElementById('class-select')
+let generateBtnEl = $('#generate-btn')
+let randomizeBtnEl = $('#randomize-btn')
+let outputEl = document.getElementById('output')
+let possibleScores = [15, 14, 13, 12, 10, 8]
+
+
+// Populate Races to Dropdown Menu
+function populateRaces() {
+
+    fetch(races)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function(data) {
+            console.log(data)
+            for (let i = 0; i < data.results.length; i++) {
+                let raceOption = document.createElement('option')
+                raceOption.classList = 'dropdown-option'
+                raceOption.value = data.results[i].index
+                console.log(raceOption.value)
+                raceOption.innerHTML = data.results[i].name
+                raceEl.appendChild(raceOption)
+            }
+        })
+let raceE1 = document.getElementById('race-select')
+
+}
+populateRaces()
+
+// Populate Classes to dropdown Menu
+function populateClasses() {
+
+    fetch(classes)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function(data) {
+            console.log(data)
+            for (let i = 0; i < data.results.length; i++) {
+                let classOption = document.createElement('option')
+                classOption.classList = 'dropdown-option'
+                classOption.value = data.results[i].index
+                console.log(classOption.value)
+                classOption.innerHTML = data.results[i].name
+                classEl.appendChild(classOption)
+            }
+        })
+
+
+}
+
+
+populateClasses()
+
+// add event listener to generate button
+generateBtnEl.click(function(){
+    fetch(abilityScores)
+        .then(function(response){
+            return response.json()
+        })
+        .then(function (data){
+            console.log(data)
+            for (let i=0; i<data.results.length; i++){
+                let ability = document.createElement('p')
+                ability.innerHTML = data.results[i].name
+                outputEl.appendChild(ability)
+            }
+        })
+
+})
+
+let characterList = document.getElementById("character-list");
 
 // saves users generated character and adds to local storage
-function saveCharacter() {
-  console.log("saveCharacter");
+function handleSaveClick() {
+    console.log("saveCharacter");
+  const characterArr = getCharacters()
+  if (characterArr.length>4) {
+    characterArr.shift()
+  }
+  console.log(characterArr)
+  let characterClass = document.getElementById("class-select");
+  let classValue = characterClass.value;
+  let characterRace = document.getElementById("race-select");
+  let raceValue = characterRace.value;
+  console.log(classValue)
+  console.log(raceValue)
+  const newCharacter = {
+    race: raceValue,
+    class: classValue
+  }
+  characterArr.push(newCharacter);
   localStorage.setItem("character", JSON.stringify(characterArr));
 }
-saveCharacter();
+
+// gets characters from local storage
+function getCharacters() {
+    const characterStorage = localStorage.getItem("character");
+    const characterArr = JSON.parse(characterStorage);
+    return characterArr || []
+}
+
+// clears saved characters from local storage
+function clearAll() {
+    localStorage.clear()
+    clearItems()
+}
+
+// clears loaded character list
+function clearItems() {
+    characterList.innerHTML='';
+}
 
 // loads saved character and their features for user
 function retrieveSavedCharacter() {
-  // localStorage.getItem('character')
+    clearItems()
+    const characterArr = getCharacters()
+  console.log(characterArr);
   console.log("retrieveSavedCharacter");
-
   for (i = 0; i < characterArr.length; i++) {
-    let character = document.getElementById("character-list");
     let savedCharacter = document.createElement("li");
-    savedCharacter.innerText = JSON.stringify(characterArr[i]);
-    character.appendChild(savedCharacter);
+    const characterText = `class: ${characterArr[i].class + ','} race: ${characterArr[i].race}`
+    savedCharacter.innerText = (characterText);
+    characterList.appendChild(savedCharacter);
   }
 }
