fd2b5420c6e1ad6469eb201ca7939174846b13fa
Duck0-Land <erhilston@gmail.com>
2023-03-09 21:26:28 -0500

    added style to avatar.html and comments to avatar.js

--- assets/js/avatar.js
+++ assets/js/avatar.js
@@ -1,16 +1,15 @@
-//randomizer variables
+//randomizer section start//
+//randomizer variables//
 let aframe = document.getElementById("frame");
 let gen = document.getElementById("gen");
 
-//randomizer section
-//avatar api randomizer function
 function generate() {
     let id = Math.random();
     let url = `https://avatars.dicebear.com/api/adventurer/${id}.svg`
     aframe.src = url;
 }
 
-//event to prevent default img
+
 gen.addEventListener('click', function(e) {
     e.preventDefault();
     generate();
