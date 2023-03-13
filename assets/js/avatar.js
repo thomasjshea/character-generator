741ecb020c79b87af2b1c945e94be2a2ef2c289a
Duck0-Land <erhilston@gmail.com>
2023-03-13 18:40:08 -0400

    updates

--- assets/js/avatar.js
+++ assets/js/avatar.js
@@ -11,9 +11,26 @@ function generate() {
     let url = `https://avatars.dicebear.com/api/adventurer/${id}.svg`
     aframe.src = url;
 }
-//event to prevent default img//
+
+gen.addEventListener('click', function(e) {
+    e.preventDefault();
+    generate();
+});
+
+
+
+//adv
+function generate() {
+    let id = Math.floor(Math.random()*4)+1;
+    let url = `https://api.dicebear.com/5.x/adventurer/svg?skinColor=9e5622,763900,ecad80`
+    aframe.src = url;
+};
+
+
 gen.addEventListener('click', function(e) {
     e.preventDefault();
     generate();
     
 });
+
+
