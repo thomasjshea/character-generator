01568c3797ac5698f41336d9447b088b228d6a64
Brain Atoms <badams6453@gmail.com>
2023-03-14 14:39:21 -0400

    disabled load button until save button is clicked

--- assets/js/avatar.js
+++ assets/js/avatar.js
@@ -1,22 +1,22 @@
-//randomizer section start//
-//randomizer variables//
-let aframe = document.getElementById("frame");
-let gen = document.getElementById("gen");
+// //randomizer section start//
+// //randomizer variables//
+// let aframe = document.getElementById("frame");
+// let gen = document.getElementById("gen");
 
 
 
-//avatar api randomizer function//
-function generate() {
-    let id = Math.random();
-    let url = `https://avatars.dicebear.com/api/adventurer/${id}.svg`
-    aframe.src = url;
-}
+// //avatar api randomizer function//
+// function generate() {
+//     let id = Math.random();
+//     let url = `https://avatars.dicebear.com/api/adventurer/${id}.svg`
+//     aframe.src = url;
+// }
 
-gen.addEventListener('click', function(e) {
-    e.preventDefault();
-    generate();
-});
+// gen.addEventListener('click', function(e) {
+//     e.preventDefault();
+//     generate();
+// });
 
 
 
-//advanced section//
+// //advanced section//
